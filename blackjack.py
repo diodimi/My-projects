402c8a1001c5f352dd0ea684f4c5ac1ec0da1323
diodimi <43845548+diodimi@users.noreply.github.com>
2018-10-18 20:25:27 +0300

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -4,28 +4,34 @@ def answer(player):
 	global list,y
 	answer=str(input('Player %d pull or pass?: ' %y))
 	sum=0
+	sum_2=0
 	for i in player:
 			sum+=dict[i[0]]
+			sum_2+=dict_2[i[0]]
 	while answer=='pull':
 		player.append(list.pop())
 		print(player)
 		sum+=dict[player[-1][0]]
+		sum_2+=dict_2[player[-1][0]]
 		if sum<21:
 			answer=str(input('Pull or pass?: '))
 		elif sum>21:
 			print('You lost :(')
 			sum= 0
 			answer=0
-	return sum
+	if sum_2<=21:
+		return sum_2
+	else:
+		return sum
 		
-			
 		
-dict={'K':10,'Q':10,'J':10,1:1,1:11,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10}
+dict={'K':10,'Q':10,'J':10,'A':1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10}
+dict_2={'K':10,'Q':10,'J':10,'A':11,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10}
 num=[]
 y=1
-for i in range(1,11):
+for i in range(2,11):
 	num.append(i)
-chars=['K','J','Q']
+chars=['A','K','J','Q']
 cards=num+chars
 colour=['K','H','S','V']
 list=[]
@@ -54,5 +60,3 @@ if answer_1!=0:
 		print('It\'s a draw!')
 else:
 	print('Player 2 wins!')
-
-
