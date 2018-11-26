0d8f3aea06440dad12b8908773032ea684bfeed9
diodimi <diodimi14@gmail.com>
2018-11-26 11:09:03 +0200

    Add files via upload

---
+++ confirm.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var name=document.getElementById("name").innerhtml;
+    var tel=document.getElementById("tel").innerhtml;
+    var question=document.getElementById("question").innerhtml;
+    var email=document.getElementById("email").innerhtml;
+}
+var name=document.getElementById("name").innerhtml;
+var tel=document.getElementById("tel").innerhtml;
+var question=document.getElementById("question").innerhtml;
+var email=document.getElementById("email").innerhtml;
+
+document.getElementById("newname").innerhtml=name;
+document.getElementById("newphone").innerhtml=tel;
+document.getElementById("newquestion").innerhtml=question;
+document.getElementById("newemail").innerhtml=email;
